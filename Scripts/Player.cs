1825cec1f35f2bb05da4bf108f5fd4cde6390b86
ivansiuzz <ivan.siu@yahoo.com>
2021-11-05 18:09:44 +0800

    add

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float mySpeed;
+    Animator myAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+     myAnim = GetComponent<Animator>();   
     }
 
     // Update is called once per frame
@@ -24,6 +25,7 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
 
+        myAnim.SetFloat("Run", Mathf.Abs(a));
         float temp = transform.position.x + a *  Time.deltaTime * mySpeed;
         transform.position = new Vector3(temp, transform.position.y, transform.position.z);
 
